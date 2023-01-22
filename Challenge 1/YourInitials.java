c8c752d24b3a0cb6b1f545d96db6418f9dec193e
SalihHakanUlufer <hakanulufer@hotmail.com>
2023-01-22 23:25:47 +0100

    1-Challenge

---
+++ Challenge 1/YourInitials.java
@@ -0,0 +1,27 @@
+public class YourInitials {
+    public static void main(String[] args) {
+      System.out.println("    SSSSSS   ");
+      System.out.println("   SS    SS  ");
+      System.out.println("   SS     S  ");
+      System.out.println("    SS       ");
+      System.out.println("        SS   ");
+      System.out.println("  S      SS  ");
+      System.out.println("   SS    SS  ");
+      System.out.println("    SSSSSS   ");
+      System.out.println("             ");
+      System.out.println("H           H");
+      System.out.println("H           H");
+      System.out.println("H           H");
+      System.out.println("HHHHHHHHHHHHH");
+      System.out.println("H           H");
+      System.out.println("H           H");
+      System.out.println("H           H");
+      System.out.println("             ");
+      System.out.println("      A      ");
+      System.out.println("     A A     ");
+      System.out.println("    A   A    ");
+      System.out.println("   AAAAAAA   ");
+      System.out.println("  A       A  ");
+      System.out.println(" A         A ");
+    }
+}
