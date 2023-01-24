c8bb0ae3758852f331e7f9f61c15f2a152eb983a
SalihHakanUlufer <hakanulufer@hotmail.com>
2023-01-24 19:24:31 +0100

    4.1-Detention

---
+++ Workbook 4.1/Detention.java
@@ -0,0 +1,32 @@
+public class Detention {
+    public static void main(String[] args) {
+
+        //what Bart did so far
+        printLines();
+        printLines();
+        printLines();
+        printLines();
+        printLines();
+        printLines();
+        // Task 2 – call the function 6 times.
+   }
+   public static void printLines() {
+
+      System.out.println("I will not copy and paste code.");
+      System.out.println("I will use Camel Case when writing class names.");
+      System.out.println("I will use lower Camel Case when writing function names.");
+      System.out.println("I will use lower Camel Case when writing variables names.\n");
+   }
+
+
+    //Task 1 - Make a function here. See the doc comment for details.
+
+    /**
+      * Funtion name: printLines
+      *
+      * Inside the function:
+      *   1. prints the four lines
+      */
+
+
+}
