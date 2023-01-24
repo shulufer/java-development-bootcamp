c9f55bb8e4f00639e3bd23b9de1c100f6f54f13d
SalihHakanUlufer <hakanulufer@hotmail.com>
2023-01-24 21:31:41 +0100

    4.3-Weather

---
+++ Workbook 4.3/Weather.java
@@ -0,0 +1,45 @@
+public class Weather {
+    public static void main(String[] args) {
+      double noon = 77;          //temperature at noon in fahrenheit.
+      double evening = 61;       //temperature during the evening in fahrenheit
+      double midnight = 55;      //temperature at midnight in fahrenheit
+
+      //Task 3 - Call the printTemperatures function.
+      printTemperatures(noon, fahrenheitToCelsius(noon));
+      printTemperatures(evening, fahrenheitToCelsius(evening));
+      printTemperatures(midnight, fahrenheitToCelsius(midnight));
+
+    }
+
+
+    //Task 1: Make a function here. See the doc comments below.
+
+    /**
+     * Function name: fahrenheitToCelsius - converts fahrenheit to celcius
+     * @param fahrenheit (double)
+     * @return celsius (double)
+     *
+     * Inside the function:
+     * 1. returns the temperature in celcius. C = (F - 32) * 5/9.
+     */
+    public static double fahrenheitToCelsius(double f) {
+      double c = (f - 32) * 5/9;
+      return c;
+    }
+
+
+    //Task 2: Make a function here. See the doc comments below.
+
+    /**
+     * Function name: printTemperatures – prints both temperature values.
+     * @param fahrenheit (double)
+     *
+     * Inside the function:
+     *  1. prints: F: <temperature in fahrenheit>.
+     *  2. prints: C: <temperature in celsius> .
+     */
+    public static void printTemperatures(double f, double c) {
+      System.out.println("F: " + f);
+      System.out.println("C: " + c);
+    }
+}
