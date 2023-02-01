c97cec63ff45d1f3cc25b13f60f31bdf3985b089
SalihHakanUlufer <hakanulufer@hotmail.com>
2023-02-01 13:34:44 +0100

    5.1-Megaphone

---
+++ Workbook 5.1/Megaphone.java
@@ -0,0 +1,9 @@
+public class Megaphone {
+    public static void main(String[] args) {
+        // See Learn the Part for the instructions.
+        for (int i=1; i<=10; i++) {
+          System.out.println(i + ")If Java was easy, they would call it Python!");
+        }
+    }
+
+}
