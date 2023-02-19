8be845c1ca9838c31a0bd26c73ade5c364b12fd9
SalihHakanUlufer <hakanulufer@hotmail.com>
2023-02-19 18:52:06 +0100

    6.1-Exams

---
+++ Workbook 6.1/Exams.java
@@ -0,0 +1,18 @@
+public class Exams {
+    public static void main(String[] args) {
+
+        // Instructions for this workbook are on Learn the Part (Workbook 6.1).
+
+        System.out.println("It's time to take your 5th year exams. Please, take your seats.\n");
+
+        String[] student = {"Harry", "Neville", "Ron", "Hermione", "Seamus"};
+        System.out.println(student[0] + ", you will take seat " + 0);
+        System.out.println(student[1] + ", you will take seat " + 1);
+        System.out.println(student[2] + ", you will take seat " + 2);
+        System.out.println(student[3] + ", you will take seat " + 3);
+        System.out.println(student[4] + ", you will take seat " + 4);
+
+        // Compare your result to what's on Learn the Part.
+
+    }
+}
