d68e9a3cdfea18be4fe5f101d5b10e05b2ed11e2
SalihHakanUlufer <hakanulufer@hotmail.com>
2023-02-21 13:39:03 +0100

    6.8-Receipt

---
+++ Workbook 6.8/Receipt.java
@@ -0,0 +1,22 @@
+public class Receipt {
+    public static void main(String[] args) {
+
+        String[] apples = {"Gala", "Granny Smith", "Macintosh"};
+        Double[] prices = {1.99, 1.49, 1.29};
+
+
+
+        System.out.println("Here's your receipt:\n");
+
+
+
+        // See instructions on Learn the Part (Workbook 6.8)
+
+
+        for (int i = 0; i < apples.length; i++) {
+
+          System.out.println("\t " + apples[i] + ": $" + prices[i]); // to be used in for loop.
+        }
+
+    }
+}
