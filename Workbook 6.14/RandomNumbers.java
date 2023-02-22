ae8906df05c39f3a1f87369facc918e8035a226d
SalihHakanUlufer <hakanulufer@hotmail.com>
2023-02-22 14:00:28 +0100

    6.14-RandomNumbers

--- Workbook 6.14/RandomNumbers.java
+++ Workbook 6.14/RandomNumbers.java
@@ -1,26 +1,53 @@
+import java.util.Arrays;
+
 public class RandomNumbers {
     public static void main(String[] args) {
 
-      double[][] prices = {
-        {12.99, 8.99, 9.99, 10.49, 11.99},
-        {0.99, 1.99, 2.49, 1.49, 2.99},
-        {8.99, 7.99, 9.49, 9.99, 10.99}
-      };
 
-      for (int i = 0; i < prices.length; i++) {
-        switch (i) {
-          case 0: System.out.print("Baking: "); break;
-          case 1: System.out.print("Beverage: "); break;
-          case 2: System.out.print("Cereals: "); break;
-          default: break;
+
+        // The instructions for this workbook are on Learn the Part (Workbook 6.14)
+
+
+        // Create a 2D array with 100 rows and 10 columns. Then call print2DArray.
+
+        int[][] hunderedRowsAndTenColumnsArray = new int[100][10];
+        for (int i = 0; i < hunderedRowsAndTenColumnsArray.length; i++) {
+          for (int j = 0; j < hunderedRowsAndTenColumnsArray[i].length; j++) {
+            hunderedRowsAndTenColumnsArray[i][j] = randomNumber();
+          }
         }
-        for (int j = 0; j < prices[i].length; j++) {
-          System.out.print(prices[i][j] + " ");
+        print2DArray(hunderedRowsAndTenColumnsArray);
+    }
+
+    /**
+     * function name: randomNumber()
+     * @return (int)randomNumber
+     * Return number between from 0 to 100.
+     */
+
+    public static int randomNumber() {
+      double randomNumber = Math.random()*100;
+      return (int)randomNumber;
+  }
+
+    /**
+     * Function name: print2DArray
+     * @param array ( int[][] )
+     *
+     * Inside the function:
+     *  1. Nested loop:
+     *      - Inner Loop: System.out.print(array[i][j] + " ");
+     *      - After the Inner Loop Completes: System.out.print("\n");
+     */
+
+     public static void print2DArray(int[][] array) {
+      for (int i = 0; i < array.length; i++) {
+        for (int j = 0; j < array[i].length; j++) {
+          System.out.print(array[i][j] + " ");
         }
         System.out.print("\n");
       }
 
-        // The instructions for this workbook are on Learn the Part (Workbook 6.14)
-    }
+     }
 
 }
