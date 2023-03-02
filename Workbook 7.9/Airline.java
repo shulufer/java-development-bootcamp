dc77b7a5e747d40b7a28e1493f1184db55c8664c
SalihHakanUlufer <hakanulufer@hotmail.com>
2023-03-02 21:36:01 +0100

    7.9-Airline Reservation

---
+++ Workbook 7.9/Airline.java
@@ -0,0 +1,31 @@
+public class Airline {
+
+    Person[] people; //array that stores Person objects...
+
+    public Airline() {
+        this.people = new Person[11];
+    }
+
+    public Person getPerson(int index) {
+        Person person = this.people[index];
+        return new Person(person); //copy constructor creates copy of object
+    }
+
+    public void setPerson(Person person) {
+        int index = person.getSeatNumber() - 1;
+        this.people[index] = new Person(person);
+    }
+
+    public void createReservation(Person person) {
+         // since seat numbers can range from 1 - 11, whereas the index ranges from 0 - 10.
+        while (people[person.getSeatNumber() - 1] !=  null) {
+          System.out.println("\n" + person.getName() + ", seat: " + person.getSeatNumber() + " is already taken. Please choose another seat.\n");
+          person.chooseSeat();
+        }
+        int index = person.getSeatNumber() - 1;
+        people[index] = new Person(person);
+        System.out.println("Thank you " + person.getName() + " for flying with Java airlines. Your seat number is " + person.getSeatNumber() + ".\n");
+    }
+
+
+}
