90050a9061066562737060f968904764753d46d6
SalihHakanUlufer <hakanulufer@hotmail.com>
2023-10-05 18:47:22 +0200

    city class created, set and get methods created, some exception created

---
+++ MapAndExceptionExtra/City.java
@@ -0,0 +1,47 @@
+
+public class City {
+
+  private String name;
+  private String country;
+  private long population;
+
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    if (name.equals(null) || (name.equals(""))) {
+      throw new IllegalArgumentException("Name cannot be null or blank.");
+    } else {
+      this.name = name;
+    }
+  }
+
+  public String getCountry() {
+    return this.country;
+  }
+
+  public void setCountry(String country) {
+    if (country.equals(null) || (country.equals(""))) {
+      throw new IllegalArgumentException("Country  cannot be null or blank.");
+    } else {
+
+      this.country = country;
+    }
+  }
+
+  public long getPopulation() {
+    return this.population;
+  }
+
+  public void setPopulation(long population) {
+    if (population <= 0) {
+      throw new IllegalArgumentException("Population must be greater than 0.");
+    } else {
+      this.population = population;
+    }
+  }
+
+
+}
