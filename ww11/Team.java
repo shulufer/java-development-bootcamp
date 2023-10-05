f7f0562eed8b31ee0cdc0df726382599bba0a11e
SalihHakanUlufer <hakanulufer@hotmail.com>
2023-10-06 00:10:38 +0200

    methods were fixed

--- ww11/Team.java
+++ ww11/Team.java
@@ -1,16 +1,18 @@
 import java.util.HashMap;
 import java.util.Map;
+import constants.Position;
+
 
 public class Team {
 
     private String name;
-    private Map<String, String> players;
+    private Map<Position, String> players;
 
     public Team(String name) {
         this.name = name;
         this.players = new HashMap<>();
     }
-    
+
     public String getName() {
         return this.name;
     }
@@ -21,15 +23,15 @@ public class Team {
         this.name = name;
     }
 
-    public String getPlayer(String position) {
+    public String getPlayer(Position position) {
         return this.players.get(position);
     }
 
-    public void setPlayer(String position, String player) {
-        if (!position.equals("SHOOTING_GUARD") || !position.equals("SMALL_FORWARD") || !position.equals("POWER_FORWARD") || !position.equals("CENTER") || !position.equals("POINT_GUARD")) {
+    public void setPlayer(Position position, String player) {
+        if (!position.equals(SHOOTING_GUARD) || !position.equals(SMALL_FORWARD) || !position.equals(POWER_FORWARD) || !position.equals(CENTER) || !position.equals(POINT_GUARD)) {
             throw new IllegalArgumentException("INVALID POSITION");
         }
         this.players.put(position, player);
     }
-    
+
 }
