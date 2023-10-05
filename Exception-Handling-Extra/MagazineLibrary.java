aa1986fa0e792f5e97145ad2b28b585e6ba52292
SalihHakanUlufer <hakanulufer@hotmail.com>
2023-10-05 10:12:24 +0200

    magazine library and constructor was created

---
+++ Exception-Handling-Extra/MagazineLibrary.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class MagazineLibrary {
+  private ArrayList<Magazine> magazines;
+
+
+  public MagazineLibrary() {
+    magazines = new ArrayList<>();
+  }
+
+}
