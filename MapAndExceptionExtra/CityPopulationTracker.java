db05e1d07e977783cc90113325259d3585ede70c
SalihHakanUlufer <hakanulufer@hotmail.com>
2023-10-05 19:28:52 +0200

    addCity method created and setCity method fixed

--- MapAndExceptionExtra/CityPopulationTracker.java
+++ MapAndExceptionExtra/CityPopulationTracker.java
@@ -14,10 +14,13 @@ public class CityPopulationTracker {
   }
 
   public void setCity(City city) {
+    cityPopulations.remove(city.getName(), city);
     cityPopulations.put(new String(city.getName()), new City(city));
   }
 
 
-
+  public void addCity(City city) {
+    cityPopulations.put(new String(city.getName()), new City(city));
+  }
 
 }
