bdfaeb8c23f8bb8cf67a430ba6e2474a40eb5c5c
SalihHakanUlufer <hakanulufer@hotmail.com>
2023-10-05 18:56:22 +0200

    constructor was created

--- MapAndExceptionExtra/CityPopulationTracker.java
+++ MapAndExceptionExtra/CityPopulationTracker.java
@@ -5,4 +5,10 @@ public class CityPopulationTracker {
   private HashMap<String, City> cityPopulations;
 
 
+  public CityPopulationTracker(HashMap<String,City> cityPopulations) {
+    this.cityPopulations = new HashMap<>();
+  }
+
+
+
 }
