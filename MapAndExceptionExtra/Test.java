bb44485a96984c2d24e92095b5827c3b504bce2e
SalihHakanUlufer <hakanulufer@hotmail.com>
2023-10-05 18:53:22 +0200

    city tested

---
+++ MapAndExceptionExtra/Test.java
@@ -0,0 +1,24 @@
+public class Test {
+
+  public static void main(String[] args) {
+    City city1 = new City("New York", "USA", 8500000);
+    System.out.println(city1.getName());
+
+
+    // Uncomment the following line to test invalid input for setName method
+    // city1.setName("");
+
+
+    // Uncomment the following line to test invalid input for setCountry method
+    // city1.setCountry("");
+
+
+    // Uncomment the following line to test invalid input for setPopulation method
+    // city1.setPopulation(-1);
+
+
+    // Uncomment the following line to test invalid input for the constructor
+    // City city2 = new City("", "USA", 8500000);
+  }
+
+}
