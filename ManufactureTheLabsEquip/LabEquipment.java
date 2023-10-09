25f29a38c74244afe4ad3f856ebcc8d25c97e696
SalihHakanUlufer <hakanulufer@hotmail.com>
2023-10-09 10:20:58 +0200

    getter and setters method created

--- ManufactureTheLabsEquip/LabEquipment.java
+++ ManufactureTheLabsEquip/LabEquipment.java
@@ -4,7 +4,44 @@ public abstract class LabEquipment {
   private String manufacturer;
   private String model;
   private int year;
+  private static final int MIN_YEAR = 1950;
 
+
+  public String getManufacturer() {
+    return this.manufacturer;
+  }
+
+  public void setManufacturer(String manufacturer) {
+    if (manufacturer ==  null|| manufacturer.equals("Manufacturer cannot be null or blank.")) {
+      throw new IllegalArgumentException();
+    } else {
+      this.manufacturer = manufacturer;
+    }
+  }
+
+  public String getModel() {
+    return this.model;
+  }
+
+  public void setModel(String model) {
+    if (model == null || model.equals("Model cannot be null or blank.")) {
+      throw new IllegalArgumentException();
+    } else {
+      this.model = model;
+    }
+  }
+
+  public int getYear() {
+    return this.year;
+  }
+
+  public void setYear(int year) {
+    if (year < MIN_YEAR) {
+      throw new IllegalArgumentException("Year must be greater than or equal to the minimum year.");
+    } else {
+      this.year = year;
+    }
+  }
 
 
 }
