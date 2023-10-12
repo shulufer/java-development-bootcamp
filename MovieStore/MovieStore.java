47c2d42130b06f25000fdf660194e4fd80019247
SalihHakanUlufer <hakanulufer@hotmail.com>
2023-10-12 16:58:51 +0200

    getTopmovies method and sortByYear method was created

--- MovieStore/MovieStore.java
+++ MovieStore/MovieStore.java
@@ -15,10 +15,23 @@ public class MovieStore {
 
   public List<Movie> filterByGenre(String genre) {
     return this.movies.stream()
-    .filter(movie -> movie.getGenre().equals(genre))
-    .toList();
+                      .filter(movie -> movie.getGenre().equals(genre))
+                      .toList();
 
 
+  }
+
+  public List<Movie> sortByReleaseYear() {
+    return this.movies.stream()
+                      .sorted((movie1, movie2) -> Integer.compare(movie1.getYear(), movie2.getYear()))
+                      .toList();
+  }
+
+  public List<Movie> getTopRatedMovies(int i) {
+    return this.movies.stream()
+                      .sorted((movie1, movie2) -> Integer.compare(movie1.getYear(), movie2.getYear()))
+                      .limit(2)
+                      .toList();
   }
 
 
